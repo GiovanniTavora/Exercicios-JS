b99e0e361ed05615ddae97bbd612ca8cb2c3b431
Giovanni Távora <giovanni15.tavora@gmail.com>
2025-03-27 11:01:42 -0300

    commit

--- Exercicios-JavaScript/src/main.js
+++ Exercicios-JavaScript/src/main.js
@@ -12,6 +12,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const numerosOriginais = document.getElementById("numerosOriginais");
   const numerosMultiplicados = document.getElementById("numerosMultiplicados");
 
+  const numerosFiltro = [5, 8, 12, 19, 3, 25, 7, 15];
+  const numerosOriginaisFilter = document.getElementById("numerosOriginaisFilter");
+  const numerosFiltrados = document.getElementById("numerosFiltrados");
+  const botaoFiltrar = document.getElementById("botaoFiltrar");
+
   const numerosSomados = document.querySelector("#numerosSomados");
   const botaoSoma = document.querySelector("#botaoSoma");
 
@@ -34,6 +39,15 @@ document.addEventListener("DOMContentLoaded", () => {
     botaoSoma.addEventListener("click", () => {
       const soma = numeros.reduce((total, num) => total + num, 0);
       numerosSomados.innerText = `Soma dos números: ${soma}`
+    });
+  }
+
+  if (numerosOriginaisFilter && numerosFiltrados && botaoFiltrar) {
+    numerosOriginaisFilter.innerText = `Array original: ${numerosFiltro.join(", ")}`;
+
+    botaoFiltrar.addEventListener("click", () => {
+      const resultado = numerosFiltro.filter(num => num > 10);
+      numerosFiltrados.innerText = `Números maiores que 10: ${resultado.join(", ")}`;
     });
   }
 
